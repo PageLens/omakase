89e0884b61dd410410b58c8c7394954fe88a8b98
Jerry Luk <jerryluk@gmail.com>
2015-01-12 21:58:48 -0800

    updated dkim.rb

--- config/initializers/dkim.rb
+++ config/initializers/dkim.rb
@@ -2,6 +2,6 @@ Dkim::domain      = 'pagelensmail.com'
 Dkim::selector    = 'default'
 Dkim::private_key = case Rails.env
 when "development", "test" then open("#{Rails.root}/keys/pagelensmail.com.dkim/default.private").read
-else open("~/keys/pagelensmail.com.dkim/default.private").read
+else open("/home/deploy/keys/pagelensmail.com.dkim/default.private").read
 end
 Dkim::signable_headers = Dkim::DefaultHeaders - %w{Message-ID Resent-Message-ID Date Return-Path Bounces-To}
